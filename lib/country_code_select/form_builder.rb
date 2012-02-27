b05c7ca4b32f2e2bbf7e85e1354180be952c2678
Brad Carson <brad@base3media.com>
2012-02-27 14:58:52 -0500

    added html_options to select

--- lib/country_code_select/form_builder.rb
+++ lib/country_code_select/form_builder.rb
@@ -1,7 +1,7 @@
 module CountryCodeSelect
 	module FormBuilder
-		def country_code_select(method, priority_countries = nil, options = {})
-			@template.country_code_select(@object_name, method, priority_countries, options.merge(object: @object))
+		def country_code_select(method, priority_countries = nil, options = {}, html_options = {})
+			@template.country_code_select(@object_name, method, priority_countries, options.merge(object: @object), html_options)
 		end
 	end
 end
--- lib/country_code_select/form_helpers.rb
+++ lib/country_code_select/form_helpers.rb
@@ -1,7 +1,7 @@
 module CountryCodeSelect
 	module FormHelpers
-		def country_code_select(object_name, method, priority_countries = nil, options = {})
-			ActionView::Helpers::InstanceTag.new(object_name, method, self, options.delete(:object)).to_country_code_select_tag(priority_countries, options)
+		def country_code_select(object_name, method, priority_countries = nil, options = {}, html_options = {})
+			ActionView::Helpers::InstanceTag.new(object_name, method, self, options.delete(:object)).to_country_code_select_tag(priority_countries, options, html_options)
 		end
 	end
 end
