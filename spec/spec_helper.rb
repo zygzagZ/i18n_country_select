47f0fadd8bfa18ebe71298efde6d0741faa62dc0
Brian McQuay <brian@onomojo.com>
2012-04-02 13:43:54 -0600

    getting tests working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,22 @@ ENV["RAILS_ENV"] = "test"
 $: << File.dirname(__FILE__) + "/../lib" << File.dirname(__FILE__)
 require 'rspec'
 require "active_support"
-require "action_view"
+require 'action_view'
 require 'i18n_country_select'
-require 'support/fake_app'
+require 'rails/all'
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.fail_fast = true
 end
+
+
+module Config
+  class Application < ::Rails::Application
+    # configuration here if needed
+    config.active_support.deprecation = :stderr
+  end
+end
+ 
+# Initialize the application
+Config::Application.initialize!
