67eb227f708f8fd0067a14e614c7494e3baff266
Andreas Wolff <rubyphunk@gmail.com>
2012-06-03 14:08:46 +0200

    Allow include_blank lines on top of prio countries

--- lib/i18n_country_select/instance_tag.rb
+++ lib/i18n_country_select/instance_tag.rb
@@ -1,7 +1,7 @@
 module I18nCountrySelect
   module InstanceTag
     include Countries
-    
+
     def to_country_code_select_tag(priority_countries, options = {}, html_options = {})
       country_code_select(priority_countries, options, html_options)
     end
@@ -9,20 +9,23 @@ module I18nCountrySelect
     # Adapted from Rails country_select. Just uses country codes instead of full names.
     def country_code_select(priority_countries, options, html_options)
       selected = object.send(@method_name) if object.respond_to?(@method_name)
-      
+
       country_translations = COUNTRY_CODES.map{|code| [I18n.t(code, :scope => :countries), code]}.sort_alphabetical_by(&:first)
 
       countries = ""
+
+      if options[:include_blank]
+        option = options[:include_blank] == true ? "" : options[:include_blank]
+        countries += "<option value=\"\">#{option}</options>\n"
+      end
+
       if priority_countries
         countries += options_for_select(priority_countries, selected)
-        countries += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
-      elsif options[:include_blank]
-        countries += "<option value=\"\">" + options[:include_blank] + "</options>\n"
         countries += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
       end
 
       countries = countries + options_for_select(country_translations, selected)
-      
+
       html_options = html_options.stringify_keys
       add_default_name_and_id(html_options)
 
